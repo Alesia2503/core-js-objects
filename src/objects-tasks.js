4100a8c43a8e3ed370b6cbbbf5e2c979f333700a
Alesia_Shikurova <ales2503@yandex.by>
2024-02-28 19:24:14 +0300

    feat: add removeProperties task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -51,8 +51,14 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const Objects = obj;
+  if (Object.keys(Objects).length !== 0) {
+    for (let i = 0; i < keys.length; i += 1) {
+      delete Objects[keys[i]];
+    }
+  }
+  return Objects;
 }
 
 /**
